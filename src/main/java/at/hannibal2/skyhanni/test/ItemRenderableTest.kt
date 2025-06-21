5e5136e383fd3a993c9266a3afa7a10fce71a34f
DavidArthurCole <davidarthurcole23@gmail.com>
2025-06-21 19:02:17 -0400

    Final changes

--- src/main/java/at/hannibal2/skyhanni/test/ItemRenderableTest.kt
+++ src/main/java/at/hannibal2/skyhanni/test/ItemRenderableTest.kt
@@ -19,7 +19,7 @@ object ItemRenderableTest {
     private val config get() = SkyHanniMod.feature.dev.debug
     private val BOX_OF_SEEDS_ITEM = "BOX_OF_SEEDS".toInternalName()
     private val itemStackRenderable by lazy {
-        ItemStackRenderable(BOX_OF_SEEDS_ITEM.getItemStack(), scale = 4.0)
+        ItemStackRenderable(BOX_OF_SEEDS_ITEM.getItemStack(), scale = 3.0)
     }
     private val animatedItemStackRenderable by lazy {
         AnimatedItemStackRenderable(
